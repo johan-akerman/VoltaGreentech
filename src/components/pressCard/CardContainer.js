7cc697167e6fc0154b69cb81d37092635c68141c
johan-akerman <joha.aker@gmail.com>
2022-02-07 18:55:38 +0100

    added new press release and articles

--- src/components/pressCard/CardContainer.js
+++ src/components/pressCard/CardContainer.js
@@ -24,7 +24,7 @@ export default function CenteredTextAndImage(props) {
         }
       }
 
-      article3: file(relativePath: { eq: "articles/solution.png" }) {
+      article3: file(relativePath: { eq: "articles/pilot.png" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid_withWebp_noBase64
@@ -42,11 +42,18 @@ export default function CenteredTextAndImage(props) {
       </Fade>
       <div className="row">
         <Card
+          gatsbyImage={data.article3.childImageSharp.fluid}
+          title="Volta Greentech’s seaweed reduces methane from cows by over 80 percent, commercial pilot shows"
+          date="2022.02.07"
+          href="https://medium.com/@VGreentech/volta-greentechs-seaweed-reduces-methane-from-cows-by-over-80-percent-commercial-pilot-shows-e692dfe51217"
+          delay="250"
+        />
+        <Card
           gatsbyImage={data.article2.childImageSharp.fluid}
           title="Volta Greentech raises 1.7 million Euro to fight burps from cows"
           date="2021.05.21"
           href="https://medium.com/@VGreentech/sweden-based-startup-volta-greentech-raises-1-7-49de3e7923c4"
-          delay="250"
+          delay="500"
         />
 
         <Card
@@ -54,14 +61,6 @@ export default function CenteredTextAndImage(props) {
           title="Volta partners with AFRY to build first seaweed factory aiming to reduce 90% of methane emissions from cows"
           date="2021.01.29"
           href="https://voltagreentech.medium.com/volta-greentech-partners-with-afry-to-build-first-seaweed-factory-aiming-to-reduce-80-of-methane-6bfb0531c0f5"
-          delay="500"
-        />
-
-        <Card
-          gatsbyImage={data.article3.childImageSharp.fluid}
-          title="Swedens largest feed company Lantmännen in partnership with Volta Greentech to develop the feed of the future"
-          date="2020.10.29"
-          href="https://voltagreentech.medium.com/swedens-largest-feed-company-lantm%C3%A4nnen-is-developing-the-feed-of-the-future-by-testing-methane-ad9062766f2e"
           delay="750"
         />
       </div>
