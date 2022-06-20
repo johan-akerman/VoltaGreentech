115c577e12f6679597c95b33e21ac863ad5367c7
johan-akerman <joha.aker@gmail.com>
2022-06-20 21:30:47 +0200

    added lome

--- src/components/pressCard/CardContainer.js
+++ src/components/pressCard/CardContainer.js
@@ -8,7 +8,7 @@ import { useStaticQuery, graphql } from "gatsby"
 export default function CenteredTextAndImage(props) {
   const data = useStaticQuery(graphql`
     query {
-      article1: file(relativePath: { eq: "articles/factory.jpg" }) {
+      article1: file(relativePath: { eq: "articles/lome.png" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid_withWebp_noBase64
@@ -42,25 +42,24 @@ export default function CenteredTextAndImage(props) {
       </Fade>
       <div className="row">
         <Card
+          gatsbyImage={data.article1.childImageSharp.fluid}
+          title="World's first methane reduced beef in Swedish supermarkets"
+          date="2022.06.21"
+          href="https://medium.com/@VGreentech/v%C3%A4rldens-f%C3%B6rsta-metanreducerade-n%C3%B6tk%C3%B6tt-nu-i-butik-d16642cd8db0"
+          delay="250"
+        />
+        <Card
           gatsbyImage={data.article3.childImageSharp.fluid}
           title="Volta Greentech’s seaweed reduces methane from cows by over 80 percent, commercial pilot shows"
           date="2022.02.07"
           href="https://medium.com/@VGreentech/volta-greentechs-seaweed-reduces-methane-from-cows-by-over-80-percent-commercial-pilot-shows-e692dfe51217"
-          delay="250"
+          delay="500"
         />
         <Card
           gatsbyImage={data.article2.childImageSharp.fluid}
           title="Volta Greentech raises 1.7 million Euro to fight burps from cows"
           date="2021.05.21"
           href="https://medium.com/@VGreentech/sweden-based-startup-volta-greentech-raises-1-7-49de3e7923c4"
-          delay="500"
-        />
-
-        <Card
-          gatsbyImage={data.article1.childImageSharp.fluid}
-          title="Volta partners with AFRY to build first seaweed factory aiming to reduce 90% of methane emissions from cows"
-          date="2021.01.29"
-          href="https://voltagreentech.medium.com/volta-greentech-partners-with-afry-to-build-first-seaweed-factory-aiming-to-reduce-80-of-methane-6bfb0531c0f5"
           delay="750"
         />
       </div>
