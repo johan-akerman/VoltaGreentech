175a8ee20dc107d9221219c2ee4c17cde27d78aa
johan-akerman <joha.aker@gmail.com>
2022-11-19 19:38:55 +0100

    added new press release

--- src/components/pressCard/CardContainer.js
+++ src/components/pressCard/CardContainer.js
@@ -16,7 +16,9 @@ export default function CenteredTextAndImage(props) {
         }
       }
 
-      article2: file(relativePath: { eq: "articles/fredrik.png" }) {
+      article2: file(
+        relativePath: { eq: "articles/fredrik_and_angelo_in_lab.png" }
+      ) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid_withWebp_noBase64
@@ -42,24 +44,25 @@ export default function CenteredTextAndImage(props) {
       </Fade>
       <div className="row">
         <Card
+          gatsbyImage={data.article2.childImageSharp.fluid}
+          title="Volta Greentech gears up: Swedish food companies join forces to reduce climate impact from cattle"
+          date="2022.11.16"
+          href="https://medium.com/@VGreentech/volta-greentech-gears-up-swedish-food-companies-join-forces-to-reduce-climate-impact-from-cattle-8abb1409531a"
+          delay="250"
+        />
+
+        <Card
           gatsbyImage={data.article1.childImageSharp.fluid}
           title="World's first methane reduced beef in Swedish supermarkets"
           date="2022.06.21"
           href="https://medium.com/@VGreentech/v%C3%A4rldens-f%C3%B6rsta-metanreducerade-n%C3%B6tk%C3%B6tt-nu-i-butik-d16642cd8db0"
-          delay="250"
+          delay="500"
         />
         <Card
           gatsbyImage={data.article3.childImageSharp.fluid}
           title="Volta Greentech’s seaweed reduces methane from cows by over 80 percent, commercial pilot shows"
           date="2022.02.07"
           href="https://medium.com/@VGreentech/volta-greentechs-seaweed-reduces-methane-from-cows-by-over-80-percent-commercial-pilot-shows-e692dfe51217"
-          delay="500"
-        />
-        <Card
-          gatsbyImage={data.article2.childImageSharp.fluid}
-          title="Volta Greentech raises 1.7 million Euro to fight burps from cows"
-          date="2021.05.21"
-          href="https://medium.com/@VGreentech/sweden-based-startup-volta-greentech-raises-1-7-49de3e7923c4"
           delay="750"
         />
       </div>
