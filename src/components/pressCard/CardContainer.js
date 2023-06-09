260c39e81c6d6e0849393769bd595cd5124ee5ce
johan-akerman <joha.aker@gmail.com>
2023-06-09 10:00:07 +0200

    added new press release

--- src/components/pressCard/CardContainer.js
+++ src/components/pressCard/CardContainer.js
@@ -26,7 +26,7 @@ export default function CenteredTextAndImage(props) {
         }
       }
 
-      article3: file(relativePath: { eq: "articles/pilot.png" }) {
+      article3: file(relativePath: { eq: "articles/lome_beef.png" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid_withWebp_noBase64
@@ -44,11 +44,18 @@ export default function CenteredTextAndImage(props) {
       </Fade>
       <div className="row">
         <Card
+          gatsbyImage={data.article3.childImageSharp.fluid}
+          title="Launch of Lome Beef at Hemköp. Probably the most climate-friendly beef on the planet."
+          date="2023.06.07"
+          href="https://medium.com/@VGreentech/press-release-2023-06-07-english-928e79a4bb97"
+          delay="250"
+        />
+        <Card
           gatsbyImage={data.article2.childImageSharp.fluid}
           title="Volta Greentech gears up: Swedish food companies join forces to reduce climate impact from cattle"
           date="2022.11.16"
           href="https://medium.com/@VGreentech/volta-greentech-gears-up-swedish-food-companies-join-forces-to-reduce-climate-impact-from-cattle-8abb1409531a"
-          delay="250"
+          delay="500"
         />
 
         <Card
@@ -56,13 +63,6 @@ export default function CenteredTextAndImage(props) {
           title="World's first methane reduced beef in Swedish supermarkets"
           date="2022.06.21"
           href="https://medium.com/@VGreentech/v%C3%A4rldens-f%C3%B6rsta-metanreducerade-n%C3%B6tk%C3%B6tt-nu-i-butik-d16642cd8db0"
-          delay="500"
-        />
-        <Card
-          gatsbyImage={data.article3.childImageSharp.fluid}
-          title="Volta Greentech’s seaweed reduces methane from cows by over 80 percent, commercial pilot shows"
-          date="2022.02.07"
-          href="https://medium.com/@VGreentech/volta-greentechs-seaweed-reduces-methane-from-cows-by-over-80-percent-commercial-pilot-shows-e692dfe51217"
           delay="750"
         />
       </div>
