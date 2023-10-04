a11b4d00a5a7c5bdfaa1667a847524c680fe9deb
johan-akerman <joha.aker@gmail.com>
2023-10-04 22:21:12 +0200

    added new press release

--- src/components/pressCard/CardContainer.js
+++ src/components/pressCard/CardContainer.js
@@ -16,9 +16,7 @@ export default function CenteredTextAndImage(props) {
         }
       }
 
-      article2: file(
-        relativePath: { eq: "articles/fredrik_and_angelo_in_lab.png" }
-      ) {
+      article2: file(relativePath: { eq: "articles/farm.png" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid_withWebp_noBase64
@@ -44,11 +42,20 @@ export default function CenteredTextAndImage(props) {
       </Fade>
       <div className="row">
         <Card
+          gatsbyImage={data.article2.childImageSharp.fluid}
+          title="Swedish government advised to feed cows methane-reducing additives to meet climate targets
+          "
+          date="2023.10.04"
+          href="https://medium.com/@VGreentech/swedish-government-advised-to-feed-cows-methane-reducing-additives-to-meet-climate-targets-8b84a0e87f21"
+          delay="250"
+        />
+
+        <Card
           gatsbyImage={data.article1.childImageSharp.fluid}
           title="Kan färre rapar minska Felix köttbullars klimatavtryck?"
           date="2023.09.18"
           href="https://medium.com/@VGreentech/kan-f%C3%A4rre-rapar-minska-felix-k%C3%B6ttbullars-klimatavtryck-42b6cead334f"
-          delay="250"
+          delay="500"
         />
 
         <Card
@@ -56,13 +63,6 @@ export default function CenteredTextAndImage(props) {
           title="Launch of Lome Beef at Hemköp. Probably the most climate-friendly beef on the planet."
           date="2023.06.07"
           href="https://medium.com/@VGreentech/press-release-2023-06-07-english-928e79a4bb97"
-          delay="500"
-        />
-        <Card
-          gatsbyImage={data.article2.childImageSharp.fluid}
-          title="Volta Greentech gears up: Swedish food companies join forces to reduce climate impact from cattle"
-          date="2022.11.16"
-          href="https://medium.com/@VGreentech/volta-greentech-gears-up-swedish-food-companies-join-forces-to-reduce-climate-impact-from-cattle-8abb1409531a"
           delay="750"
         />
       </div>
