adea5e7d3af0b450d7746d2e91f114f9417a4d89
johan-akerman <joha.aker@gmail.com>
2024-03-13 19:37:49 +0100

    removed job ad for cultivation

--- src/components/jobsContainer/JobsContainer.js
+++ src/components/jobsContainer/JobsContainer.js
@@ -17,14 +17,14 @@ export default function NewsContainer(props) {
       </Fade>
 
       <div className="row" style={{ marginTop: "72px" }}>
-        {/* <p>Sorry, we have no job openings right now. </p> */}
+        <p>Sorry, we have no job openings right now. </p>
 
-        <JobCard
+        {/* <JobCard
           jobTitle="Cultivation Technician Internship"
           location="Lysekil, Sweden"
           href="/open-positions/cultivation_technician_internship"
           delay="000"
-        />
+        /> */}
       </div>
     </div>
   )
