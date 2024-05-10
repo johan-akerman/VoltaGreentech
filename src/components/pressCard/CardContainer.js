78d71b2f879e8d1d21e2d81c199dd4437d0d89d0
johan-akerman <joha.aker@gmail.com>
2024-05-10 09:24:12 +0200

    new press release

--- src/components/pressCard/CardContainer.js
+++ src/components/pressCard/CardContainer.js
@@ -8,7 +8,7 @@ import { useStaticQuery, graphql } from "gatsby"
 export default function CenteredTextAndImage(props) {
   const data = useStaticQuery(graphql`
     query {
-      article1: file(relativePath: { eq: "articles/orkla_collaboration.png" }) {
+      article1: file(relativePath: { eq: "articles/gotland.png" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid_withWebp_noBase64
@@ -24,7 +24,9 @@ export default function CenteredTextAndImage(props) {
         }
       }
 
-      article3: file(relativePath: { eq: "articles/uk.png" }) {
+      article3: file(
+        relativePath: { eq: "articles/next generation lome.png" }
+      ) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid_withWebp_noBase64
@@ -42,26 +44,26 @@ export default function CenteredTextAndImage(props) {
       </Fade>
       <div className="row">
         <Card
-          gatsbyImage={data.article2.childImageSharp.fluid}
-          title="Denmark's strategy for a world-first tax in agriculture"
-          date="2024.02.29"
-          href="https://medium.com/@VGreentech/on-wednesday-last-week-a-long-awaited-report-commissioned-by-the-danish-government-was-released-c6c21f0223f8"
+          gatsbyImage={data.article3.childImageSharp.fluid}
+          title="Next Generation Lome: Volta Greentech raises 32 million SEK investment to bring cost-efficient and scalable climate solution to agriculture"
+          date="2024.05.10"
+          href="https://medium.com/@VGreentech/a18a6da790d4"
           delay="250"
         />
 
         <Card
-          gatsbyImage={data.article3.childImageSharp.fluid}
-          title="UK Government plowing ahead with methane-suppressing feed products for cows"
-          date="2023.11.01"
-          href="https://medium.com/@VGreentech/uk-government-plowing-ahead-with-methane-suppressing-feed-products-for-cows-e07179ac0245"
+          gatsbyImage={data.article1.childImageSharp.fluid}
+          title="Smak av Gotland — soon the most climate-friendly beef on the market"
+          date="2024.03.27"
+          href="https://medium.com/@VGreentech/smak-av-gotland-soon-the-most-climate-friendly-beef-on-the-market-1a2af1eced9a"
           delay="500"
         />
 
         <Card
-          gatsbyImage={data.article1.childImageSharp.fluid}
-          title="Kan färre rapar minska Felix köttbullars klimatavtryck?"
-          date="2023.09.18"
-          href="https://medium.com/@VGreentech/kan-f%C3%A4rre-rapar-minska-felix-k%C3%B6ttbullars-klimatavtryck-42b6cead334f"
+          gatsbyImage={data.article2.childImageSharp.fluid}
+          title="Denmark's strategy for a world-first tax in agriculture"
+          date="2024.02.29"
+          href="https://medium.com/@VGreentech/on-wednesday-last-week-a-long-awaited-report-commissioned-by-the-danish-government-was-released-c6c21f0223f8"
           delay="750"
         />
       </div>
